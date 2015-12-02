3b724f7ffb0d1a7f68682ff61835a147dbed3c46
j-t-s <j-t-s@users.noreply.github.com>
2015-12-02 13:46:58 -0500

    Update NodeHandler.java
    Made the mode and all the methods static so they can be accessed without an instance of the class.
    For example, to create a node simply call...
    NodeHandler.create(NodeType);//NodeType could be an enum or simply a string such as "and"

--- src/NodeHandler.java
+++ src/NodeHandler.java
@@ -1,9 +1,17 @@
 public class NodeHandler{
   enum Mode {MOVE, SELECT, TOGGLE_POWER};
-  Mode mode;
+  private static Mode mode;
+  
+  static Mode getMode(){
+    return mode;
+  }
+  
+  static void setMode(Mode newMode){
+    mode = newMode;
+  }
   
   /**Constructs a new note determined by type and appends it to the Node linkedList in GateOr.*/
-  /*void create(NodeType type){}//enum NodeType will need to be defined.
+  /*static void create(NodeType type){}//enum NodeType will need to be defined.
     Node newOne;
     switch(type){
       case Power: newOne = new Power();break;
@@ -18,24 +26,24 @@ public class NodeHandler{
     GateOr.NodeList.add(newOne);
   */
   /**Retrieves nodes from the selected node list to remove all reference of them found in the node linked list and the input lists of all of the node.*/
-  void delete(){}
+  static void delete(){}
   /**Updates the coordinates of the nodes in the selected nodes list based on the user's mouse coordinates*/
-  void move(){
-    mode = Mode.MOVE;
+  static void move(){
+    setMode(Mode.MOVE);
     //Among other things.
   }
   /**Adds or removes the nodes found in the selected node list based on the user mouse input*/
-  void select(){
-    mode = Mode.SELECT;
+  static void select(){
+    setMode(mode = Mode.SELECT);
     //Among other things.
   }
   /**This method takes the first two nodes defined in the selected node list and adds the first node's reference to the second node's input list*/
-  void connect(){}
+  static void connect(){}
   /**Uses the nodes in the selected node list to remove all references of each other in their input lists*/
-  void disconnect(){}
+  static void disconnect(){}
   /**Toggles the power for all Power nodes in the selected node list*/
-  void togglePower(){
-    mode = Mode.TOGGLE_POWER;
+  static void togglePower(){
+    setMode(mode = Mode.TOGGLE_POWER);
     //Among other things.
   }
 }

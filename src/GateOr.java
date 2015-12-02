6d4d0c908058247770c4447ecc06ae3f0e5de35e
j-t-s <j-t-s@users.noreply.github.com>
2015-12-01 20:53:58 -0500

    Update GateOr.java
    Added the fields and methods.
    I'll get to the build methods later when the renderer and nodes are starting to be implemented.

--- src/GateOr.java
+++ src/GateOr.java
@@ -1,8 +1,18 @@
 public class GateOr{
+	LinkedList<Node> NodeList;
+	LinkedList<Node> SelectedList;
+	
 	public static void main(String args[]){
-	
-	System.out.println("blah blah");
-	
+		System.out.println("blah blah");
+		build();
+		buildGUI();
+		//plus more possibly
 	}
+	/**Build and run the GUI elements, this includes starting the GUI's thread and renderer*/
+	void buildGUI(){}
+	/**Will build and initialize all necessary data structures*/
+	void build(){}
+	LinkedList<Node> getNodeList(){return NodeList;}
+	LinkedList<Node> getSelectedList(){return SelectedList;}
 
 }

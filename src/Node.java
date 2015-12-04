b5e78eee83d03b27014f14ba2aadd268cc613139
j-t-s <j-t-s@users.noreply.github.com>
2015-12-04 12:40:00 -0500

    Update Node.java
    Added the State enum

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,5 @@
 public abstract class Node{
-  public enum Type {POWER, OUTPUT, AND, OR, XOR, NOR, NAND, NOT}
+  public enum Type {POWER, OUTPUT, AND, OR, XOR, NOR, NAND, NOT};
+  public enum State {ON, OFF, UNDEF};
   
 }

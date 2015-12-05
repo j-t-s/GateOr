b6586279c4210c8a2fe2a9a25ac7032ef666f32a
j-t-s <j-t-s@users.noreply.github.com>
2015-12-05 18:18:54 -0500

    Update Node.java
    added methods to interact with input and state.
    still needs coordinates and name.
    The updateState method needs uncommented when it is implemented in the subclasses.

--- src/Node.java
+++ src/Node.java
@@ -2,4 +2,33 @@ public abstract class Node{
   public enum Type {POWER, OUTPUT, AND, OR, XOR, NOR, NAND, NOT};
   public enum State {ON, OFF, UNDEF};
   
+  State state = State.UNDEF;
+  
+  /**
+	Sets a non-null input to the Node and returns true. If there are no non-null inputs no input is et and returns false
+	Gate class use this to set the inputs list.
+	Output class use this to set the input reference.
+	Power implement this method by always returning false.
+  */
+  public abstract boolean setInput(Node addedInput);
+  
+  /**
+	Gets the inputs at a certain index.
+	Gate class use this to get an input from the inputs list.
+	Output class use this to get the input reference.
+	Power implement this method by always null.
+  */
+  public abstract Node getInput(int index);
+  /**
+	Clear the inputs at a certain index.
+	Gate class use this to clear an input from the inputs list.
+	Output class use this to clear the input reference.
+	Power implement this method by being empty.
+  */
+  public abstract void clrInput(int index);
+  
+  public State getState(){return state;}
+  protected void setState(State state){this.state = state;}
+  //public abstract void updateState();//
+  
 }

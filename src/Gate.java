344087510382adfcd4b29737f708c2c68e65c0ee
j-t-s <j-t-s@users.noreply.github.com>
2015-12-05 18:13:53 -0500

    Update Gate.java

--- src/Gate.java
+++ src/Gate.java
@@ -1,1 +1,10 @@
-public abstract class Gate extends Node{}
+public abstract class Gate extends Node{
+	public boolean setInput(Node addedInput){//This is just a stub
+		return false;
+	}
+	public Node getInput(int index){//Currently a stub
+		return null;
+	}
+	public void clrInput(int index){//Currently a stub
+	}
+}

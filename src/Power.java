aee456f03340e9f331946fddd25bf100ff49fe6c
j-t-s <j-t-s@users.noreply.github.com>
2015-12-05 18:22:32 -0500

    Update Power.java

--- src/Power.java
+++ src/Power.java
@@ -1,1 +1,9 @@
-public class Power extends Node{}
+public class Power extends Node{
+  public boolean setInput(Node addedInput){
+		return false;
+	}
+	public Node getInput(int index){
+		return null;
+	}
+	public void clrInput(int index){}
+}

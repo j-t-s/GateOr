a895ba9c25bc38c54d7836434ab7a4b129979879
j-t-s <j-t-s@users.noreply.github.com>
2015-12-05 18:21:40 -0500

    Update Output.java

--- src/Output.java
+++ src/Output.java
@@ -1,1 +1,10 @@
-public class Output extends Node{}
+public class Output extends Node{
+  	public boolean setInput(Node addedInput){//This is just a Stub
+		return false;
+	}
+	public Node getInput(int index){//This is a stub
+		return null;
+	}
+	public void clrInput(int index){//Currently a stub
+	}
+}

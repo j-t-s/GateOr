48bfdf3cecdaaef2cd2828b941bd89e978bb765c
j-t-s <j-t-s@users.noreply.github.com>
2015-12-07 17:20:25 -0500

    Update AND.java

--- src/AND.java
+++ src/AND.java
@@ -1,7 +1,7 @@
 public class AND extends Gate{
-  public AND(Point coord, String name){super(coord, name);}
+  public AND(java.awt.Point coord, String name){super(coord, name);}
   @Override
-  void updateState(){
+  public void updateState(){
     if (this.getInput(0).getState() == Node.State.ON && this.getInput(1).getState() == Node.State.ON){//Both are ON, so state is ON
       this.setState(Node.State.ON);
     }else if (this.getInput(0).getState() == Node.State.OFF || this.getInput(1).getState() == Node.State.OFF){//One of them is OFF, so off
@@ -11,11 +11,11 @@ public class AND extends Gate{
     }
   }
   @Override
-  void draw(java.awt.Graphics g){
+  public void draw(java.awt.Graphics g){
     java.awt.image.BufferedImage img = null;
     String filename = "AND.png";
     //get the image
-    try {img = javax.imageio.ImageIO.read(new java.io.File(filename));}catch (IOException e){System.out.println("Could not get "+filename);}
+    try {img = javax.imageio.ImageIO.read(new java.io.File(filename));}catch (java.io.IOException e){System.out.println("Could not get "+filename);}
     //draw the image
     g.drawImage(img, getLocation().x, getLocation().y, null);
   }

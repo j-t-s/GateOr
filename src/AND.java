698fc23ea93fc855d73811de81bf15663798a613
j-t-s <j-t-s@users.noreply.github.com>
2015-12-07 16:22:32 -0500

    Update AND.java, Draw and updateState method
    I am going to compile the code so far and start to work on the rendererer.

--- src/AND.java
+++ src/AND.java
@@ -1,3 +1,22 @@
 public class AND extends Gate{
   public AND(Point coord, String name){super(coord, name);}
+  @Override
+  void updateState(){
+    if (this.getInput(0).getState() == Node.State.ON && this.getInput(1).getState() == Node.State.ON){//Both are ON, so state is ON
+      this.setState(Node.State.ON);
+    }else if (this.getInput(0).getState() == Node.State.OFF || this.getInput(1).getState() == Node.State.OFF){//One of them is OFF, so off
+      this.setState(Node.State.OFF);
+    }else{//at least one is undefined and at most one is ON.
+      this.setState(Node.State.UNDEF);
+    }
+  }
+  @Override
+  void draw(java.awt.Graphics g){
+    java.awt.image.BufferedImage img = null;
+    String filename = "AND.png";
+    //get the image
+    try {img = javax.imageio.ImageIO.read(new java.io.File(filename));}catch (IOException e){System.out.println("Could not get "+filename);}
+    //draw the image
+    g.drawImage(img, getLocation().x, getLocation().y, null);
+  }
 }

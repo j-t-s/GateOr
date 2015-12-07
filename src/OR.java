5e7a16d8e85ec52f5dc31a30a3feed9d6d846b9c
j-t-s <j-t-s@users.noreply.github.com>
2015-12-07 17:22:04 -0500

    Update OR.java

--- src/OR.java
+++ src/OR.java
@@ -1,9 +1,9 @@
 public class OR extends Gate{
-  public OR(Point coord, String name){super(coord, name);}
+  public OR(java.awt.Point coord, String name){super(coord, name);}
   @Override
-  void updateState(){//needs implemented
+  public void updateState(){//needs implemented
     
   }
   @Override
-  void draw(java.awt.Graphics g){}//needs implemented
+  public void draw(java.awt.Graphics g){}//needs implemented
 }

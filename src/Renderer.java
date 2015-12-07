b1a3bf604bc8f38978d8b201398e35b81befda5d
j-t-s <j-t-s@users.noreply.github.com>
2015-12-07 18:44:53 -0500

    Update Renderer.java

--- src/Renderer.java
+++ src/Renderer.java
@@ -1,3 +1,36 @@
-public class Renderer{
+import java.util.LinkedList;
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Graphics;
+import java.awt.Color;
 
+public class Renderer extends JPanel implements ActionListener{
+	private Dimension windowSize;
+	private LinkedList<Node> nodeList;
+	
+	public Renderer(LinkedList<Node> nlist){
+		this.nodeList = nlist;
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		this.repaint();
+	}
+	
+	public Dimension getPreferredSize(){
+		return new Dimension(854,640);
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		for (int i = 0; i < nodeList.size(); i++){
+			nodeList.get(i).draw(g);
+		}
+		for (int i = 0; i < GateOr.getSelectedList().size(); i++){
+			Node node = GateOr.getSelectedList().get(i);
+			g.setColor(Color.BLACK);
+			g.drawString("Selected", node.getLocation().x, node.getLocation().y);
+		}	
+	}
 }

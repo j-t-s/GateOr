357fccec4cc5b7355994935320f427db9af7e5a2
j-t-s <j-t-s@users.noreply.github.com>
2015-12-12 21:41:08 -0500

    Update NOR.java

--- src/NOR.java
+++ src/NOR.java
@@ -1,9 +1,7 @@
 public class NOR extends Gate{
-  public NOR(java.awt.Point coord, String name){super(coord, name);}
+  public NOR(java.awt.Point coord, String name){super(coord, name);this.imgName = "NOR.png";}
   @Override
   public void updateState(){//needs implemented
     
   }
-  @Override
-  public void draw(java.awt.Graphics g){}//needs implemented
 }

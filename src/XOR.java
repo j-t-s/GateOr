9a0f82634c8153f97e256a9c80ff9a17c9667458
j-t-s <j-t-s@users.noreply.github.com>
2015-12-12 21:39:25 -0500

    Update XOR.java

--- src/XOR.java
+++ src/XOR.java
@@ -1,9 +1,7 @@
 public class XOR extends Gate{
-  public XOR(java.awt.Point coord, String name){super(coord, name);}
+  public XOR(java.awt.Point coord, String name){super(coord, name);this.imgName = "XOR.png";}
   @Override
   public void updateState(){//needs implemented
     
   }
-  @Override
-  public void draw(java.awt.Graphics g){}//needs implemented
 }

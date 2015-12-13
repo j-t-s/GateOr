1f444b78f80d75ca4d5a1f0fb832eccf99e7b750
j-t-s <j-t-s@users.noreply.github.com>
2015-12-12 21:41:26 -0500

    Update NAND.java

--- src/NAND.java
+++ src/NAND.java
@@ -1,16 +1,7 @@
 public class NAND extends Gate{
-  public NAND(java.awt.Point coord, String name){super(coord, name);}
+  public NAND(java.awt.Point coord, String name){super(coord, name);this.imgName = "NAND.png";}
   @Override
   public void updateState(){//needs implemented
     
-  }
-  @Override
-  public void draw(java.awt.Graphics g){//needs implemented
-	java.awt.image.BufferedImage img = null;
-    String filename = "NAND.png";
-    //get the image
-    try {img = javax.imageio.ImageIO.read(new java.io.File(filename));}catch (java.io.IOException e){System.out.println("Could not get "+filename);}
-    //draw the image
-    g.drawImage(img, getLocation().x, getLocation().y, null);
   }
 }
